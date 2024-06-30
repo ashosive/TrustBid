6e34049a33bc270a5e9e3bb59e6f0de27e774693
Sufiyan <sufiyanmemon687@gmail.com>
2024-06-30 02:02:23 -0400

    added market activities and user history apis

--- backend/src/api/routes/event.route.js
+++ backend/src/api/routes/event.route.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const { eventDecodeController } = require('../controllers/event.controller');
+const { eventDecodeController, eventLatestInteractionsController, eventAllLatestInteractionsController } = require('../controllers/event.controller');
 const router = express.Router();
 
 router.post('/event/decode',eventDecodeController);
+
+router.get('/event/interactions/latest',eventLatestInteractionsController);
+
+router.get('/event/interactions/all/',eventAllLatestInteractionsController)
 
 module.exports = router;
